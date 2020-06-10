18de72ec4696303fec7bbde2b27d1269a1952106
csunklee <65987216+csunklee@users.noreply.github.com>
2020-06-09 23:44:10 -0700

    Update UserWon.java

--- CODE/UserWon.java
+++ CODE/UserWon.java
@@ -1,37 +1,19 @@
 import java.io.*;
-import java.net.*;
-import java.util.*;
 
-/**
-  * ISTE 121 - CANDYLAND
-  * UserWon class.
-  *
-  * @author Regina Bass
-  * @author Miki Mian
-  * @author Amy Tan
-  * @version 1.0, 120219
-  */
 class UserWon implements Serializable {
-    
-    // attributes
+
     private User currentPlayer;
-    
-    /** 
-      * UserWon constructor.
-      *
-      * @param currentPlayer - User
-      */
+
     public UserWon(User currentPlayer) {
-    
         this.currentPlayer = currentPlayer;
     }
-     
+
     /**
-      * UserWon toString method.
-      * @return formatted winner message
-      */
+     * UserWon toString method.
+     * @return formatted winner message
+     */
     public String toString() {
-            
-        return currentPlayer.getUsername() + " won!";
-    }     
+        return currentPlayer.getUsername() + " is the winner!";
+    }
+
 }
