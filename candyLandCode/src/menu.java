6f95397ecc616c919c13807016c414dfe293ad24
Ching0509 <65987134+Ching0509@users.noreply.github.com>
2020-06-26 18:35:05 -0700

    Menu update
    Background works
    Window resolution: 911x600
    BGM added (Kirby & The Amazing Mirror's main menu BGM)
    functional JButton added 
    
    working on JButtons with graphics
    working on "RULES" page

--- candyLandCode/src/menu.java
+++ candyLandCode/src/menu.java
@@ -1,20 +1,26 @@
 import java.awt.Color;
-import java.awt.Container;
 import java.awt.Font;
+import java.awt.Image;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.net.URL;
 
+import javax.sound.sampled.AudioInputStream;
+import javax.sound.sampled.AudioSystem;
+import javax.sound.sampled.Clip;
+import javax.swing.ImageIcon;
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.JLabel;      
 
 public class menu extends JFrame{
-   
-	JFrame window;
-	Container con;
-	JPanel titleNamePanel;
-	JLabel titleNameLabel;
+	
+	public static void main(String[] args) {
+		
+		new menu();
+	}
+	
 	JPanel startButtonPanel;
 	JPanel helpButtonPanel;
 	JPanel exitButtonPanel;
@@ -22,7 +28,6 @@ public class menu extends JFrame{
 	JButton helpButton;
 	JButton exitButton;
 	
-	Font titleFont = new Font("Time New Roman", Font.PLAIN, 90);
 	Font StartFont = new Font("Time New Roman", Font.PLAIN, 30);
 	Font HelpFont = new Font("Time New Roman", Font.PLAIN, 30);
 	Font ExitFont = new Font("Time New Roman", Font.PLAIN, 30);
@@ -31,41 +36,48 @@ public class menu extends JFrame{
 	TitleScreenHandlerH tsHandlerH = new TitleScreenHandlerH();
 	TitleScreenHandlerE tsHandlerE = new TitleScreenHandlerE();
 	
-	public static void main(String[] args) {
-		
-		new menu();
-	}
-	
 	public menu() {
 		
-		window = new JFrame();
-		window.setSize(911,600);
-		window.setLayout(null);
-		window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		window.getContentPane().setBackground(Color.black);		
-		window.setVisible(true);
-		con = window.getContentPane();
+		try {
+			URL url = menu.class.getResource("BGM.wav");
+			AudioInputStream audio = AudioSystem.getAudioInputStream(url);
+			Clip music = AudioSystem.getClip();
+			music.open(audio);
+			music.loop(-1);
+		} catch (Exception ex) {}
 		
+		//frame
+		JLabel window;
+		setSize(911,600);
+		setLayout(null);
+		setDefaultCloseOperation(EXIT_ON_CLOSE);
+		//background
+		ImageIcon background_image = new ImageIcon("menu.png");
 		
-		titleNamePanel = new JPanel();
-		titleNamePanel.setBounds(100, 50, 600, 120);
-		titleNamePanel.setBackground(Color.black);
-		titleNameLabel = new JLabel("CandyLand LA");
-		titleNameLabel.setForeground(Color.white);
-		titleNameLabel.setFont(titleFont);
+		Image img = background_image.getImage();
+		Image temp_img = img.getScaledInstance(911,600,Image.SCALE_SMOOTH);
+		window = new JLabel("",background_image,JLabel.CENTER);
+		
+        window.setBounds(0, 0, 911, 600);
+		add(window);
+		
+        setVisible(true);
 		
 		startButtonPanel = new JPanel();
-		startButtonPanel.setBounds(300, 200, 200, 100);
-		startButtonPanel.setBackground(Color.black);
+		startButtonPanel.setBounds(355, 250, 200, 100);
+		startButtonPanel.setBackground(new Color(0,0,0,0));
+		window.add(startButtonPanel);
 		
 		startButton = new JButton("START");
 		startButton.setBackground(Color.black);
 		startButton.setForeground(Color.white);
 		startButton.setFont(StartFont);
+		startButtonPanel.add(startButton);	
 		
 		helpButtonPanel = new JPanel();
-		helpButtonPanel.setBounds(300, 300, 200, 100);
-		helpButtonPanel.setBackground(Color.black);
+		helpButtonPanel.setBounds(355, 350, 200, 100);
+		helpButtonPanel.setBackground(new Color(0,0,0,0));
+		window.add(helpButtonPanel);
 		
 		helpButton = new JButton("HELP");
 		helpButton.setBackground(Color.black);
@@ -73,8 +85,9 @@ public class menu extends JFrame{
 		helpButton.setFont(HelpFont);
 		
 		exitButtonPanel = new JPanel();
-		exitButtonPanel.setBounds(300, 400, 200, 100);
-		exitButtonPanel.setBackground(Color.black);
+		exitButtonPanel.setBounds(355, 450, 200, 100);
+		exitButtonPanel.setBackground(new Color(0,0,0,0));
+		window.add(exitButtonPanel);
 		
 		exitButton = new JButton("EXIT");
 		exitButton.setBackground(Color.black);
@@ -82,15 +95,10 @@ public class menu extends JFrame{
 		exitButton.setFont(HelpFont);
 		exitButton.addActionListener(tsHandlerE);
 		
-		titleNamePanel.add(titleNameLabel);
+		
 		startButtonPanel.add(startButton);
 		helpButtonPanel.add(helpButton);
 		exitButtonPanel.add(exitButton);
-		
-		con.add(titleNamePanel);
-		con.add(startButtonPanel);
-		con.add(helpButtonPanel);
-		con.add(exitButtonPanel);
 	}
 	
 	public class TitleScreenHandlerS implements ActionListener {
